eb0b84a21333fa55838cc752bd087218f8545a9d
xbmqzj <52390284+xbmqzj@users.noreply.github.com>
2020-07-23 08:38:07 +0800

    增加了一个4g方法

--- scripts/test_network.py
+++ scripts/test_network.py
@@ -25,5 +25,11 @@ class TestNetwork:
        self.network_page.click_network()
        self.network_page.click_first_network()
        self.network_page.click_3g()
+        
+    def test_network_4g(self):
+       self.network_page.click_more()
+       self.network_page.click_network()
+       self.network_page.click_first_network()
+       self.network_page.click_3g()
 
 
